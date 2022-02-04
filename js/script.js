d6e7f63f0a4317db167bc86640e81ee3c3c78274
skvoruha <skvoruha@mail.ru>
2022-02-04 23:43:35 +0300

    fix getServicePrices on getAllServicePrices

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@
 let title =  prompt("Как называется ваш проект?");
 let screens  = prompt("Какие типы экранов нужно разработать? (пример:'Простые, Сложные, Интерактивные')");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
-let rollback  = 20;
 let adaptive = confirm("Нужен ли адаптив на сайте?");
+
 let service1 = prompt("Какой дополнительный тип услуги нужен ?")
 let servicePrice1 = +prompt("Сколько это будет стоить?")
 let service2 = prompt("Какой дополнительный тип услуги нужен ?")
 let servicePrice2 = +prompt("Сколько это будет стоить?")
+
+let rollback  = 20;
 let allServicePrices;
 let fullPrice;
 let servicePercentPrice;
@@ -29,7 +31,7 @@ const getRollbackMessage = function(price){
   }
 }
 
-const getServicePrices = function (){
+const getAllServicePrices = function (){
  return servicePrice1 + servicePrice2;
 }
 
@@ -46,7 +48,7 @@ function getServicePercentPrices(){
   return fullPrice - Math.round(fullPrice * (rollback/100));
 }
 
-allServicePrices = getServicePrices();
+allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
 servicePercentPrice  = getServicePercentPrices();
 
