85bb8117be77ae8f7ec7cc60149abe5561d2407d
skvoruha <skvoruha@mail.ru>
2022-02-10 18:13:30 +0300

    add const and get elemts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,45 @@
 'use strict'
 
+// урок 9
+//1 получение заголовка методом getElementsByTagName -> элемент
+const title = document.getElementsByTagName('h1')[0]
+//2 Для получение кнопок сначала получим все элементы с классом handler_btn
+const btnCalculate = document.getElementsByClassName('handler_btn')
+//2  btnCalculate обращаемся к элементам с id start  reset
+const btnStart = btnCalculate.start
+const btnReset = btnCalculate.reset
+//3  общаемся через querySelector по классу .screen-btn к кнопке плюс
+let btnPlus = document.querySelector('.screen-btn')
+//4 Получаем все элементы с классом .other-items.percent
+const percent = document.querySelectorAll('.other-items.percent')
+//4 Получаем все элементы с классом .other-items.number
+const number = document.querySelectorAll('.other-items.number')
+//5 обращаемя к родителю  .rollback > input с типом range
+const rollbackInput = document.querySelector('.rollback input[type="range"]')
+//6 обращаемя к родителю  .rollback а далеее к span с классом  span.range-value
+const spanRangeValue = document.querySelector('.rollback span.range-value')
+//7 Получаем все input с классом total-input'
+const totalInput = document.getElementsByClassName('total-input')
+//8 Получаем все эдементы с класом  screen
+const screenBlock = document.querySelectorAll('.screen')
+
+
+// Вывод заголовка 1
+console.log(title);
+// Вывод в консоль кнопок расчитать и сброс
+console.log(btnStart);
+console.log(btnReset);
+console.log(percent);
+console.log(number);
+console.log(rollbackInput);
+console.log(spanRangeValue);
+// Вывод в консоль total-input
+for (const iterator of totalInput) {
+  console.log(iterator);
+}
+console.log(screenBlock);
+
+
 // создадим объект
 const appData = {
  title: '',
@@ -126,3 +166,4 @@ const appData = {
 }
 
 appData.start()
+
